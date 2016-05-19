6222485621569fd171769f7431983cf02a83bb36
Donovan An <donovantaean@yahoo.com>
2016-05-19 11:02:36 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var path = require("path");
+
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '/client/views')));
+app.use(express.static(path.join(__dirname, '/client/static')));
+
+// require('./server/config/mongoose.js');
+// require('./server/config/routes.js')(app);
+
+
+
+
+
+
+// Setting our Server to Listen on Port: 8000
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+});
